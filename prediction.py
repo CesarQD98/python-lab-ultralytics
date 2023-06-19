6854a9394f8325c61b378635965e4d96cada963f
Rico98 <asd.cqd98@gmail.com>
2023-06-19 09:46:54 -0500

    Updated gitignore

--- prediction.py
+++ prediction.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     model = YOLO("yolov8n-seg.pt")
     model = YOLO("./runs/segment/train3/weights/best.pt")
 
-    results = model.predict(source="test2.png", save=True)
+    results = model.predict(source="bus.jpg", save=True)
