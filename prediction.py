dff94462b687b03b1ddee24a95e987b2a8fb7e94
Rico98 <asd.cqd98@gmail.com>
2023-06-21 16:39:21 -0500

    Added video saving feature

--- prediction.py
+++ prediction.py
@@ -1,13 +1,27 @@
 import cv2
 from ultralytics import YOLO
 
+import utils
+
 if __name__ == "__main__":
     model = YOLO("yolov8n-seg.pt")
-    model = YOLO("./weights/best.pt")
+    model = YOLO("./weights_techinn/best.pt")
 
-    video_path = "./media/test.mp4"
+    video_path = "./media/mass-dropped.mp4"
 
     cap = cv2.VideoCapture(video_path)
+
+    frame_width = int(cap.get(3))
+    frame_height = int(cap.get(4))
+    frame_size = (frame_width, frame_height)
+    fps = 30
+
+    output = cv2.VideoWriter(
+        "./media/" + utils.date_filename_generator(),
+        cv2.VideoWriter_fourcc("m", "p", "4", "v"),
+        fps,
+        frame_size,
+    )
 
     while cap.isOpened():
         success, frame = cap.read()
@@ -17,6 +31,8 @@ if __name__ == "__main__":
 
             annotated_frame = results[0].plot()
 
+            output.write(annotated_frame)
+
             cv2.imshow("Test en video mp4", annotated_frame)
 
             if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -25,4 +41,5 @@ if __name__ == "__main__":
             break
 
     cap.release()
+    output.release()
     cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -2,6 +2,8 @@ from ultralytics import YOLO
 import cv2
 import numpy
 
+from datetime import datetime
+
 
 def save_images(res: list, img: numpy.ndarray):
     for result in res:
@@ -10,3 +12,7 @@ def save_images(res: list, img: numpy.ndarray):
             r = box.xyxy[0].astype(int)
             crop = img[r[1] : r[3], r[0] : r[2]]
             cv2.imwrite(str(i) + ".jpg", crop)
+
+
+def date_filename_generator():
+    return datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".mp4"
